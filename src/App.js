c6c1ab86d35dcffd44816f093f489caf5abacc8e
Jay Lee <leeja744@student.otago.ac.nz>
2022-08-10 19:29:12 +1200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import logo from './logo.svg';
-import './App.css';
 
 function App() {
+  async function fetchData() {
+    const response = await fetch('/data/nzbird.json');
+    if (!response.ok) {
+      console.error(response.status); // error handling
+    }
+    const data = response.json()
+    console.log(data); // use the data
+  }
+
+  fetchData();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+      <div>
+        <p>hello world!</p>
+      </div>
       </header>
+
+      <main>
+        <div>
+          <button>This is in the main</button>
+        </div>
+      </main>
+
+      <footer>
+        <div>
+          <p>Hello I am the footer hehe</p>
+        </div>
+      </footer>
     </div>
   );
+
 }
 
 export default App;
